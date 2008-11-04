db24803b515de6801ab09904a59f08d129bcb976
Tim Parkin <tim@ubuntu804desktop.localdomain>
2008-11-04 19:47:37 +0000

    updated migration stuff

--- scripts/migrateBloggerEntriesAndComments.py
+++ scripts/migrateBloggerEntriesAndComments.py
@@ -26,6 +26,8 @@ def charReplacements(contents):
     return contents
 
 def replaceImg(post):
+    if not hasattr('post.a', 'img'):
+        return None
     img = post.a.img
     if img is not None:
         src = img.get('src')
@@ -35,22 +37,25 @@ def replaceImg(post):
         return None
 
 def replaceTag(target, targettag,tag=None):
-    if len(targettag) >1:
-        elems = target.findAll(targettag[0],targettag[1])
-    else:
-        elems = target.findAll(targettag[0])
-    for element in elems:
-        soup = BeautifulStoneSoup()
-        contents = element.renderContents()
-        contents = charReplacements(contents)
-        if tag is None:
-            element.replaceWith(contents)
-        elif tag is 'newline':
-            element.replaceWith('\n%s'%contents)
+    try:
+        if len(targettag) >1:
+            elems = target.findAll(targettag[0],targettag[1])
         else:
-            t = Tag(soup, tag)
-            t.insert(0,contents)
-            element.replaceWith( t )
+            elems = target.findAll(targettag[0])
+        for element in elems:
+            soup = BeautifulStoneSoup()
+            contents = element.renderContents()
+            contents = charReplacements(contents)
+            if tag is None:
+                element.replaceWith(contents)
+            elif tag is 'newline':
+                element.replaceWith('\n%s'%contents)
+            else:
+                t = Tag(soup, tag)
+                t.insert(0,contents)
+                element.replaceWith( t )
+    except:
+        print target
     return
 
 
@@ -82,16 +87,16 @@ def getComments(id):
 
 def parsePost(post):
     img = replaceImg(post)
-    id = post.previous.previous.previous.previous.get('name')
+    id = post.previous.previous.previous.previous.previous.previous.previous.previous.previous.previous.get('name')
     commentUrl =  post.findNextSibling('div',{'class':'post-footer-line post-footer-line-1'}).find('a',{'class':'comment-link'}).get('href')
-    replaceTag(post.p,('span',{'style':'font-style: italic;'}),tag='em')
-    replaceTag(post.p,('span',{'style':'font-weight: bold;'}),tag='strong')
-    replaceTag(post.p,('span',{'style':'font-size:85%;'}))
-    replaceTag(post.p,('span',{'class':'blsp-spelling-error'}))
-    replaceTag(post.p,('span',{'class':'blsp-spelling-corrected'}))
-    replaceTag(post.p,('br',),tag='newline')
+    replaceTag(post,('span',{'style':'font-style: italic;'}),tag='em')
+    replaceTag(post,('span',{'style':'font-weight: bold;'}),tag='strong')
+    replaceTag(post,('span',{'style':'font-size:85%;'}))
+    replaceTag(post,('span',{'class':'blsp-spelling-error'}))
+    replaceTag(post,('span',{'class':'blsp-spelling-corrected'}))
+    replaceTag(post,('br',),tag='newline')
     date = post.findNextSibling('div',{'class':'post-footer-line post-footer-line-1'}).find('abbr').get('title')
-    text = charReplacements(''.join([str(c) for c in post.p.contents]).replace('</ul><ul>','').replace('<em></em>',''))
+    text = charReplacements(''.join([str(c) for c in post.contents]).replace('</ul><ul>','').replace('<em></em>',''))
     comments = getComments(id)    
     return {'date':date,'text':text,'img':img,'comments':comments,'id':id}
     
@@ -102,16 +107,6 @@ def getAllData():
     
     soup = BeautifulStoneSoup(html,convertEntities=BeautifulStoneSoup.HTML_ENTITIES)
     posts = soup.findAll('div',{ "class" : "post-body entry-content" })
-    #for post in posts:
-        #postData = parsePost(post)
-        #print '<h2>',postData['date'],'</h2>'
-        #print '<div>',markdown(postData['text']),'</div>'
-        #print '<dl>'
-        #if postData['comments'] is not None:
-            #for comment in postData['comments']:
-                #print '<dt>',comment['name'],'<strong>',comment['date'],'</strong></dt>'
-                #print '<dd>',markdown(comment['body']),'</dd>'
-        #print '</dl>'
                 
     
     data = []
@@ -122,6 +117,7 @@ def getAllData():
         d['body'] = text
         d['shortdescription'] = text[:150]
         d['sidebar'] = ''
+        #print 'httrack --get "https://www.blogger.com/comment.g?blogID=4047028985894596901&postID=%s&isPopup=true" -V "mv \$0  %s.html"'%(d['id'],d['id'])
         data.append(d)
 
     PYTHONPATH="PYTHONPATH=.:../share/eggs/poop-0.9.2-py2.5.egg:../share/eggs/purepg-0.6.7-py2.4.egg:../share:../share/eggs/formal-0.15.4-py2.5.egg:../share/eggs/crux-0.3.2-py2.4.egg"
@@ -129,13 +125,16 @@ def getAllData():
     from datetime import datetime
     for d in data:
         dataTemplate="from dateutil.parser import *\nimport formal\nattrs={'title':{'en':'%(title)s'},'date':parse('%(date)s'), 'shortdescription':{'en':'%(shortdescription)s'},'body':{'en':formal.types.RichText('markdown','%(body)s')},'sidebar':{'en':'%(sidebar)s'}}"%(d)
+        print dataTemplate
         f = open('buildAttrs.py','w')
         f.write(dataTemplate)
         f.close()
-        command = "%s python createitem.py -u into-the-light -d into-the-light -t basiccms.blogentry.BlogEntry --execfile=buildAttrs.py name='%s' author='Tim Parkin'"%(PYTHONPATH,d['id'])
+
+        command = "%s python createitem.py -u timparkin -d timparkin -t basiccms.blogentry.BlogEntry --execfile=buildAttrs.py name='%s' author='Tim Parkin'"%(PYTHONPATH,d['id'])
         print command
+        import sys; sys.exit()
         out = commands.getstatusoutput(command)
-        id = out[1].split('id=')[1]
+        #id = out[1].split('id=')[1]
         #import sys
         #sys.exit()
         #id = 999
@@ -147,7 +146,7 @@ def getAllData():
                 f = open('buildAttrs.py','w')
                 f.write(dataTemplate)
                 f.close()
-                command = "%s python createitem.py -u into-the-light -d into-the-light -t comments.model.Comment --execfile=buildAttrs.py %s"%(PYTHONPATH,args)
+                command = "%s python createitem.py -u timparkin -d timparkin -t comments.model.Comment --execfile=buildAttrs.py %s"%(PYTHONPATH,args)
                 print command
                 #import sys
                 #sys.exit()
